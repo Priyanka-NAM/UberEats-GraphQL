78fe212e0e8c4e57d1e1a5f47e238f6bc33c85ef
Priyanka NAM <87109108+Priyanka-NAM@users.noreply.github.com>
2022-01-27 22:17:11 -0800

    Delete config.js

--- backend/Utils/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://ubereats:ubereats@cluster0.i5aas.mongodb.net/ubereats?retryWrites=true&w=majority",
-  secret: "cmpe273_secret_key",
-  frontEnd: "http://localhost:3000",
-};
